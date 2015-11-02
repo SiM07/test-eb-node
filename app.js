c059d8f7cbb32d3a2b0f0f017926e110ebd9e3c9
Simon Rolland <srolland@20minutes.fr>
2015-11-02 20:55:33 +0100

    get 20minutes

--- app.js
+++ app.js
@@ -6,14 +6,21 @@ app.get('/', function (req, res) {
 	  res.send('Hello World! YEAH !');
 });
 
+app.get('/20minutes', function(req, res) {
+  request({
+    uri: "http://www.sitepoint.com",
+  }, function(error, response, body) {
+    req.send(body);
+  });
+});
 
 app.use(express.static('public'));
 
 var server = app.listen(process.env.PORT || 3000, function () {
-	  var host = server.address().address;
-	    var port = server.address().port;
+    var host = server.address().address;
+    var port = server.address().port;
 
-	      console.log('Example app listening at http://%s:%s', host, port);
+    console.log('Example app listening at http://%s:%s', host, port);
 });
 
 
