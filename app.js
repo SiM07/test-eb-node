a7794e7207dcda8afdfc0e8a11632570d42fbeb7
Simon Rolland <srolland@20minutes.fr>
2015-11-02 21:56:20 +0100

    add cheerio to insert script

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 var express = require('express');
 var app = express();
 var request = require('request');
+var cheerio = require("cheerio");
 
 app.get('/', function (req, res) {
 	  res.send('Hello World! YEAH !');
 });
 
 app.get('/20minutes', function (req, res) {
-  request("http://www.20minutes.fr/", function(error, response, body) {
+  request("http://www.20minutes.fr/sport/football/1722139-20151102-ligue-champions-madrid-raphael-varane-enfin-trouve-place", function(error, response, body) {
     if (!error && response.statusCode == 200) {
-       res.send(body);
+       var $ = cheerio.load(body);
+       $('#xiti-logo').before('<script>alert("Random number ' + Math.floor((Math.random() * 100) + 1) + '")</script>');
+       res.send($.html()+'</html>');
     } else {
        res.send("Error" + response.statusCode);
     }
