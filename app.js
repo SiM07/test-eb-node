f45c5766db05062e37cda711186335d17a35436e
Simon Rolland <srolland@20minutes.fr>
2015-11-03 13:10:33 +0100

    update

--- app.js
+++ app.js
@@ -13,7 +13,8 @@ app.get('/20minutes/:id', function (req, res) {
   request("http://www.20minutes.fr/sport/football/1722139-20151102-ligue-champions-madrid-raphael-varane-enfin-trouve-place", function(error, response, body) {
     if (!error && response.statusCode == 200) {
        var $ = cheerio.load(body);
-       $('#xiti-logo').before('<script>alert("Random number ' + Math.floor((Math.random() * 100) + 1) + '")</script>');
+       $('#xiti-logo').before('<style>.hallotoolbar { z-index:999 }</style><link type="text/css" href="/jquery-ui/themes/base/jquery-ui.css" rel="stylesheet" /><link rel="stylesheet" href="/font-awesome/css/font-awesome.min.css"><script type="text/javascript" src="/jquery/jquery.min.js"></script><script type="text/javascript" src="/jquery-ui/ui/minified/jquery-ui.min.js"></script><script type="text/javascript" src="/rangy-official/rangy-core.js"></script><script type="text/javascript" src="/hallo/dist/hallo.js"></script><script type="text/javascript" src="/editor.js"></script>');
+
        res.send($.html()+'</html>');
     } else {
        res.send("Error" + response.statusCode);
@@ -22,6 +23,7 @@ app.get('/20minutes/:id', function (req, res) {
 });
 
 app.use(express.static('public'));
+app.use(express.static('bower_components'));
 
 var server = app.listen(process.env.PORT || 3000, function () {
     var host = server.address().address;
